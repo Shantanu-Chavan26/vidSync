9033978468a20398283783b903cc09aaa69193e5
nandinisahu407 <nandinisahu407@gmail.com>
2024-04-28 11:43:32 +0530

    finally chat feature done bhai

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useEffect, useState } from 'react';
 import {
+  Call,
   CallControls,
   CallParticipantsList,
   CallStatsButton,
@@ -12,7 +13,8 @@ import {
 import { useRouter, useSearchParams } from 'next/navigation';
 import { Users, LayoutList } from 'lucide-react';
 
-import { useCreateChatClient, Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+import { useCreateChatClient, Chat, Channel,ChannelList, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+
 
 import {
   DropdownMenu,
@@ -26,7 +28,7 @@ import EndCallButton from './EndCallButton';
 import { cn } from '@/lib/utils';
 import Navbar from './Navbar';
 import { useUser } from '@clerk/nextjs';
-import { User, Channel as StreamChannel } from 'stream-chat';
+import { User, Channel as StreamChannel, DefaultGenerics, StreamChat } from 'stream-chat';
 
 import 'stream-chat-react/dist/css/v2/index.css';
 import './layout.css';
@@ -35,61 +37,63 @@ type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
 const MeetingRoom = () => {
 
+  const { user } = useUser();
+
+
   const searchParams = useSearchParams();
   const isPersonalRoom = !!searchParams?.get('personal');
+
+  const url = window.location.href; 
+
+  let meetingId:string;
+  if (url.includes('?personal=true')) {
+    // Personal meeting URL format
+    const parts = url.split('/');
+    meetingId = parts[parts.length - 2]; // Extract ID before "?personal=true"
+  } 
+  else {
+    const parts = url.split('/');
+    meetingId = parts[parts.length - 1]; // Extract ID after "meeting/"
+  }
+
   const router = useRouter();
   const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
   const [showParticipants, setShowParticipants] = useState(false);
-  const { useCallCallingState } = useCallStateHooks();
-  
+
+  const { useCallCallingState,useCallMembers,
+    useDominantSpeaker,
+    useParticipants,
+    useLocalParticipant,
+    useIsCallRecordingInProgress,} = useCallStateHooks();
+
+  const allParticipants=useParticipants();
+
+
+  const callingState = useCallCallingState();
+  // const [channel, setChannel] = useState<StreamChannel>();
+  const [channel, setChannel] = useState();
+
+
+  // const [client, setClient] = useState<StreamChat<DefaultGenerics> | null>(null);
+  const [client, setClient] = useState(null);
+
   const { StreamChat } = require('stream-chat');
 
 
-  // const [userToken, setUserToken] = useState(null);
-  // const [client, setClient] = useState<typeof StreamChat>(); 
-
-  const { user } = useUser();
-
-  const callingState = useCallCallingState();
-
     //chat feature
-  const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY|| '';
-  const userId = user?.id||'';
-  const userName = user?.fullName||'';
-  const userImg=user?.imageUrl;  
-  const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidXNlcl8yZkVuazRQbng2Q1lENmd2bXFIYnEzdzBGRG0ifQ.Vhc0rIQ5OzhIABRmv2kPymEeufdgOAAhuurw48NyjHI';
+    const jwt_token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidXNlcl8yZkVuazRQbng2Q1lENmd2bXFIYnEzdzBGRG0ifQ.Vhc0rIQ5OzhIABRmv2kPymEeufdgOAAhuurw48NyjHI";
 
-  const [channel, setChannel] = useState<StreamChannel>();
+    const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY|| '';
+    const userId = user?.id||'';
+    const userName = user?.fullName||'';
+    const userImg=user?.imageUrl; 
+    let usertoken:string='';
 
-  // const getToken = async () => {
-  //       try {
-  //         // Make API call to your backend to generate Stream Chat token
-  //         const response = await fetch('/api/generateToken', {
-  //           method: 'POST',
-  //           headers: {
-  //             'Content-Type': 'application/json',
-  //           },
-  //           body: JSON.stringify({ userId }),
-  //         });
-    
-  //         const token = await response.json();
-  //         console.log("user token frontend-> ", token);
-  //         setUserToken(token);
+    const filters = { type: 'messaging', members: {$in: [user?.id]}  };
+    const options = { state: true, presence: true, limit: 10 };
+    const sort = { last_message_at: -1, updated_at: -1 };
 
-  //       } catch (error) {
-  //         console.error('Error generating user token:', error);
-  //       }
-  // };
 
-  const client = useCreateChatClient({
-      apiKey,
-      tokenOrProvider: userToken,
-      userData: {
-        id: userId,
-        name: userName,
-        image: userImg,
-      },
-  });  
 
 
   const CallLayout = () => {
@@ -102,23 +106,86 @@ const MeetingRoom = () => {
         return <SpeakerLayout participantsBarPosition="right" />;
     }
   };
+
   
-  useEffect(() => {
-    if (!client) return;
 
-    const channel = client.channel('messaging','hello' , {
-      image: 'https://getstream.io/random_png/?name=vidsync',
-      name: 'Lets talk',
-      members: [userId],
-    });
+  const createChannel = async () => {
+    
+    const chatClient=new StreamChat.getInstance(apiKey);
 
-    setChannel(channel);
-  }, [client]);
+    await chatClient.connectUser({ id: userId, name:userName ,image:userImg }, chatClient.devToken(userId));
+
+    const participantPromises = allParticipants.map(async (participant) => participant.userId); 
+    const participantUserIds = await Promise.all(participantPromises); // Wait for all IDs to resolve
+    console.log("participants ids-> ",participantUserIds);
+
+    const members = participantUserIds.map(String); 
+
+    console.log("members id-> ",members);
+
+    // const messageRole = await chatClient.createRole({
+    //   name: 'chatMember',
+    //   permissions: ['ReadChannel', 'SendMessage'],
+    // });
+
+    // const roles = members.map(() => messageRole); // Array of custom roles
+
+    try {
+
+      // if(userId!='user_2fEnk4Pnx6CYD6gvmqHbq3w0FDm'){
+      //   const response = await checkChannelExists(meetingId);
+      //   console.log("other user channel-> ",channel);
+      //   setClient(chatClient);
+      //   return;
+      // }
+
+      console.log("helloooo");
+
+        const newchannel = await chatClient.channel('messaging', meetingId, {
+        image: 'https://getstream.io/random_png/?name=vidsync',
+        name: 'Lets talk',
+      });
+
+      await newchannel.addMembers(members,{role: "users"});
 
 
-  if (callingState !== CallingState.JOINED) return <Loader />;
+      console.log("new channel-> ",newchannel);
 
-  console.log("user-> ",user);
+      // await newchannel.addMembers(members);
+      setChannel(newchannel);
+
+      // await newchannel.watch();
+      setClient(chatClient);
+
+      
+
+     
+    } catch (error) {
+      console.error('Error creating channel:', error);
+    }
+    
+  };
+
+
+  useEffect(()=>{
+    async function init(){
+     
+      await createChannel(); 
+
+    }
+    init();
+
+  },[allParticipants.length,user]);
+
+  
+
+
+  if (callingState !== CallingState.JOINED || !user) return <Loader />;
+
+  // console.log("user-> ",user);
+  // console.log("metting id: ",meetingId);
+  // console.log("joined users-> ",allParticipants);
+  // console.log("channel-> ",channel)
 
   return (
     <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
@@ -171,8 +238,23 @@ const MeetingRoom = () => {
 
         {/* <Chat client={client!}>Chat with client is ready!</Chat> */}
 
-        {/* {client && channel &&( */}
-          <Chat client={client!}>
+        {client && channel && (
+          <Chat client={client!} theme='messaging '>
+          <Channel channel={channel}>
+            <Window>
+              <ChannelHeader />
+              <MessageList />
+              <MessageInput focus />
+            </Window>
+            <Thread />
+          </Channel>
+        </Chat>
+        )}    
+        
+{/* 
+        <Chat client={client!}>
+          <ChannelList filters={filters} options={options} showChannelSearch sort={sort}  />
+
           <Channel channel={channel}>
             <Window>
               <ChannelHeader />
@@ -181,9 +263,8 @@ const MeetingRoom = () => {
             </Window>
             <Thread />
           </Channel>
-        </Chat>
-        {/* )} */}
-       
+        </Chat> */}
+      
         
       </div>
     </section>
