a93dab313a3795622b27f16cab981ae07396dca3
nandinisahu407 <nandinisahu407@gmail.com>
2024-11-20 10:29:58 +0530

    updated name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="flex justify-center items-center h-12 bg-dark-1 text-white text-2xl font-bold">
-      Made by Nandini and Shantanu 
+      Made by Nandini ,Shantanu ,Sanika 
     </footer>
   );
 };
